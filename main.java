0143f821023a1b2c35b1cd9eab66f9f7670156a0
manvitha-1212 <67196221+manvitha-1212@users.noreply.github.com>
2020-10-11 14:19:00 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+class Main {
+
+public static void main(String[] args){
+Employee e1=new Employee();
+Employee e2 = new Employee();
+e1.name="Aditya";
+e1.city="mumbai";
+e1.age=19;
+e2.name="mansi";
+e2.city="delhi";
+e2.age=26;
+e1.display();
+e2.display();
+
+}
+}
